6c94d6881bc6a7cdb38eba1c30b71036fa7e59cd
Gleb <shukhleev.talan@gmail.com>
2025-10-28 17:16:09 +0500

    fixed migration

--- database/migrations/2025_10_27_000502_create_activity_organization_table.php
+++ database/migrations/2025_10_27_000502_create_activity_organization_table.php
@@ -10,7 +10,7 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void {
-        Schema::create('activity_organization', function (Blueprint $table) {
+        Schema::create('organization_activity', function (Blueprint $table) {
             $table->id();
             $table->foreignId('activity_id')->constrained()->cascadeOnDelete();
             $table->foreignId('organization_id')->constrained()->cascadeOnDelete();
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('activity_organization');
+        Schema::dropIfExists('organization_activity');
     }
 };
